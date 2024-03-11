dae786059b60357ffaa6b18953d199172abb8c8a
ashokyadavgummidi <162982065+ashokyadavgummidi@users.noreply.github.com>
2024-03-11 16:00:33 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,114 @@
+#print("hello world")
+"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+"""
+1.}commenting line is used for making documentation in python file
+2.}when the line is commented,that line will not be considered as a part of the code
+3.}when the program is excecuted,commented line will not be in the part of the excecution
+.....>note:
+	single line comminting can be done using #at the begining of the line
+	eg;#print("hello world")
+....>multiple lines connecting can be done using the line b/w qutations will be connect
+"""
+"""
+eg;
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+#print("helloworld")
+'''
+import this
+print(this)
+'''
+#import is a keyword which is used to communicated between the python files.
+
+#import antigravity
+
+#theory part
+"""
+bargained characters
+ controls and data structures.....>ABC language
+ strings and its formats....>from C
+ regular expressions....>peral
+ exception handling.....>module3
+ magic methods from.....>fortron
+ """
+"""
+what kind of language is python
+   python is a
+             ..>object oriented programing language
+             ..>dynamically type language
+             ..>intrpreted language
+             ..>compiled language
+             """
+'''
+flavours of python
+...>CPYTHON-written in C,most common implementation of python.
+
+...>JYTHON-java implementation of python.
+
+...>ironpython-implementation of the python programming langiuage which is tightily integrated with .NET
+
+...>brython -browser python ,runs in browser
+
+...>rubypython -bridge between python and ruby interpreters
+
+...>micropython -runs in micro controllers.
+'''
+"""
+print has 5 parameters---->(sep,end,file,object,flush)
+object--->value to be printed is object
+sep--->seperator
+eg;
+a=8
+b=9
+c=89
+print(a,b,c,  sep='$')
+end--->which is used to add the string at the end of the value ,
+it will not keep the command to stay in same line
+eg;
+print(str(a),b,end='%%')
+print(str(c))
+"""
+#!------>variables
+# varable is a name which is used to identify the value of varable
+#which is stored in a specific memory location
+
+#rules to define variables
+"""
+1.)varable have to start with only alphabets,underscores
+2.)the char following the 1st char can be alphabets,numbers ,underscores
+3.)no spaces are allowed
+4.)no special characters like @#$%^&*()+
+5.)case sensitive
+6.)keywords are not allowed as varables
+"""
+ways to define variables
+
+a=8
+b=9
+print(a,b)
+a=7, b=9 #error
+print(a,b)
+a,b+7,9
+print(a)
+print(b)
+a=7
+b=7
+a=b=7
+print(a,b)
+a=8,7,9.89,"hello"
+print(a)
+print(type(a))
+a,b=78 #error
+print(a,b)
